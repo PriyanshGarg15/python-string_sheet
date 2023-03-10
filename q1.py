a02f590d9efa63d78ec6aece4050d90d0574b61e
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2023-03-10 20:03:08 +0530

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,10 @@
+# s1=input()
+# l= []
+# for character in s1:
+#     if character not in l:
+#         n=s1.count(character)
+#         l.append(character)
+#         l.append(n)
+    
+# for i in range(0,len(l),2):
+#     print(l[i],"comes",l[i+1],"times")
